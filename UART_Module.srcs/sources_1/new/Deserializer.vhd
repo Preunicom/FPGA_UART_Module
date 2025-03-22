fd04e19d35fb770a66a9ecbce21ac49059ab823b
Preunicom <44133097+Preunicom@users.noreply.github.com>
2025-03-22 17:24:50 +0100

    Refactored and created TB for Deserializer

--- UART_Module.srcs/sources_1/new/Deserializer.vhd
+++ UART_Module.srcs/sources_1/new/Deserializer.vhd
@@ -1,6 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.std_logic_unsigned.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity Deserializer is
   Generic(
@@ -21,37 +21,51 @@ end Deserializer;
 
 architecture Behavioral of Deserializer is
   signal reg : std_logic_vector(DATA_BITS+STOP_BITS+PARITY_ACTIVE downto 0);
-  signal counter : std_logic_vector(3 downto 0) := "0000";
+  signal counter : integer := DATA_BITS+STOP_BITS+PARITY_ACTIVE+1;
   signal stop_bits_suffix : std_logic_vector(STOP_BITS-1 downto 0) := (others => '1');
+  signal last_serial_in : std_logic := '1';
 begin
 
   DESER: process(clk, rst)
     variable parity : std_logic;
   begin
     if rst = '1' then
+      -- Clear intern information
       parity := '0';
+      counter <= DATA_BITS+STOP_BITS+PARITY_ACTIVE+1;
       reg <= (others => '0');
-      counter <= (others => '0');
+      -- Clear outputs
       parallel_out <= (others => '0');
       data_valid <= '0';
       frame_error <= '0';
       parity_error <= '0';
     elsif rising_edge(clk) then
+      -- Set default values
+      --> Valid output for only one clock cycle 
       parity := '0';
       parallel_out <= (others => '0');
       data_valid <= '0';
       frame_error <= '0';
       parity_error <= '0';
-      counter <= counter + 1;
+      -- Shift the shift register (LSB --> shift right and put input to highest bit)
       reg <= serial_in & reg(DATA_BITS+STOP_BITS+PARITY_ACTIVE downto 1);
-      if counter = 0 then
-        if serial_in = '1' then
-            counter <= (others => '0');
-        end if;
+      -- If counter greater as the amount of UART message bits this means it was already outputted
+      --> Do not count until there is a new start bit
+      if counter <= DATA_BITS+STOP_BITS+PARITY_ACTIVE then
+        -- Count the counter
+        counter <= counter + 1;
+      end if; -- These two if have to be seperated to be able to receive a start bit directly after a stop bit
+      if counter > DATA_BITS+STOP_BITS+PARITY_ACTIVE and last_serial_in = '1' and serial_in = '0' then
+        -- UART start bit detected (falling edge on serial in)
+        --> Reset counter to 1
+        counter <= 1;
       end if;
+      -- If counter is bigger than UART message bits this means it was already outputted. 
       if counter = DATA_BITS+STOP_BITS+PARITY_ACTIVE then
+        -- Set outputs
         parallel_out <= reg(DATA_BITS+1 downto 2); -- Last Bit not shifted yet (this clock cyle shift) and start bit as offset 
         data_valid <= '1';
+        -- Calculate frame error
         if STOP_BITS = 1 then
           -- Only one Bit (serial_in) is stop bit
           if serial_in = stop_bits_suffix(0) then
@@ -68,6 +82,7 @@ begin
             frame_error <= '1';
           end if;
         end if;
+        -- Calculate parity error
         if PARITY_ACTIVE = 1 then
             -- Calculate parity incl. parity bit
             for i in 2 to DATA_BITS+2 loop
@@ -84,10 +99,17 @@ begin
             -- no parity
             parity_error <= '0';
           end if;
-        counter <= (others => '0');
       end if;
     end if;
   end process;
 
+  EDGE_DETECTION: process(clk, rst)
+  begin
+    if rst = '1' then
+      last_serial_in <= '0';
+    elsif rising_edge(clk) then
+      last_serial_in <= serial_in;
+    end if;
+  end process;
 
 end Behavioral;
