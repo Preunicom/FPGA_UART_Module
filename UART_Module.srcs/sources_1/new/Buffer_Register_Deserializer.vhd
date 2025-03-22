0f9e03b8df52dd65629b0fc030ea8a8a6d0d3e17
Preunicom <44133097+Preunicom@users.noreply.github.com>
2025-03-22 17:51:56 +0100

    Refactored and created Testbench of Buffer Register Deserializer

--- UART_Module.srcs/sources_1/new/Buffer_Register_Deserializer.vhd
+++ UART_Module.srcs/sources_1/new/Buffer_Register_Deserializer.vhd
@@ -25,19 +25,23 @@ begin
   BUFDES: process(clk, rst)
   begin
     if rst = '1' then
-      data <= (others => '0');
+      -- Clear ouputs
       parallel_out <= (others => '0');
-      frame_error_buf <= '0';
-      parity_error_buf <= '0';
       frame_error_out <= '0';
       parity_error_out <= '0';
       new_data <= '0';
+      -- Clear intern data
+      data <= (others => '0');
+      frame_error_buf <= '0';
+      parity_error_buf <= '0';
     elsif rising_edge(clk) then
+      -- Set outputs
       new_data <= '0';
       parallel_out <= data;
       frame_error_out <= frame_error_buf;
       parity_error_out <= parity_error_buf;
       if write_en = '1' then
+        -- Set new register data 
         new_data <= '1';
         data <= parallel_in;
         parallel_out <= parallel_in;
